0a6bab58fa86ad77e05673cff62fd456964206df
ngimbisarah243 <sarahngimbi@icloud.com>
2025-03-21 23:14:28 +0100

    create new additional files

---
+++ BACKEND/api/routes/userRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+//TODO
+// import { login } from '../controllers/userController'; kommt noch
+
+const router = Router();
+
+
+// TODO
+// router.post('/login', login);
+
+export default router;
