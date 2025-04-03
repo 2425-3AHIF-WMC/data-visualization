21fac47a54b07c3fcb86c0a9658c5de6c571064a
Lena Graßauer <116261857+LenaGrassauer@users.noreply.github.com>
2025-04-03 08:43:05 +0200

    richtiges pieChart.js

--- FRONTEND/D3/pieChart/pieChart.js
+++ FRONTEND/D3/pieChart/pieChart.js
@@ -1,32 +1,34 @@
-import * as d3 from "d3";
-
-export class PieChart {
-    createPieChart() {
-        const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
-        const svgWidth = 500, svgHeight = 300;
-        const radius = Math.min(svgWidth, svgHeight) / 2;
-
-        const svg = d3.select('.bar-chart')
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PieChart = void 0;
+var d3 = require("d3");
+var PieChart = /** @class */ (function () {
+    function PieChart() {
+    }
+    PieChart.prototype.createPieChart = function () {
+        var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+        var svgWidth = 500, svgHeight = 300;
+        var radius = Math.min(svgWidth, svgHeight) / 2;
+        var svg = d3.select('.bar-chart')
             .attr("width", svgWidth)
             .attr("height", svgHeight)
             .append("g")
-            .attr("transform", `translate(${svgWidth / 2}, ${svgHeight / 2})`);
-
-        const color = d3.scaleOrdinal(d3.schemeCategory10);
-        const pie = d3.pie<number>;
-        const arc = d3.arc<d3.PieArcDatum<number>>()
+            .attr("transform", "translate(".concat(svgWidth / 2, ", ").concat(svgHeight / 2, ")"));
+        var color = d3.scaleOrdinal(d3.schemeCategory10);
+        var pie = d3.pie();
+        var arc = d3.arc()
             .innerRadius(0)
             .outerRadius(radius);
-
-        const arcs = svg.selectAll(".arc")
+        var arcs = svg.selectAll(".arc")
             .data(pie(dataset))
             .enter()
             .append("g")
             .attr("class", "arc");
-    }
-}
-
+    };
+    return PieChart;
+}());
+exports.PieChart = PieChart;
 document.addEventListener("DOMContentLoaded", function () {
-    const pieChart = new PieChart();
+    var pieChart = new PieChart();
     pieChart.createPieChart();
 });
