1dc4eb80b833fad0a1d5666cf737e990039be7a2
Lena Graßauer <116261857+LenaGrassauer@users.noreply.github.com>
2025-04-10 09:26:01 +0200

    es geeeeeeeeht nicht

--- FRONTEND/D3/pieChart/pieChart.ts
+++ FRONTEND/D3/pieChart/pieChart.ts
@@ -1,74 +1,45 @@
-/*import * as d3 from "d3";
+import * as d3 from "d3";
 
-class PieChart {
+export class PieChart {
     createPieChart() {
         const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
-        const svgWidth = 500, svgHeight = 300;
-        const radius = Math.min(svgWidth, svgHeight) / 2;
 
-        const svg = d3.select('.pie-chart')
-            .attr("width", svgWidth)
-            .attr("height", svgHeight)
-            .append("g")
-            .attr("transform", `translate(${svgWidth / 2}, ${svgHeight / 2})`);
+        const width = 500;
+        const height = 300;
+        const radius = Math.min(width, height) / 2;
 
         const color = d3.scaleOrdinal(d3.schemeCategory10);
-        const pie = d3.pie<number>();
-        const arcbe  = d3.arc<d3.PieArcDatum<number>>()
+
+        const svg = d3.select(".pie-chart")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+        const pie = d3.pie<number>().value(d => d);
+
+        const arc = d3.arc<d3.PieArcDatum<number>>()
             .innerRadius(0)
             .outerRadius(radius);
 
-        const arcs = svg.selectAll(".arc")
+        const arcs = svg.selectAll("arc")
             .data(pie(dataset))
             .enter()
             .append("g")
             .attr("class", "arc");
+
+        arcs.append("path")
+            .attr("d", arc)
+
+        var text = svg.selectAll("text")
+            .attr("text-anchor", "middle")
+            .attr("font-size", "12px")
+            .attr("fill", "#fff")
+
     }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     const pieChart = new PieChart();
     pieChart.createPieChart();
-});
-
-export default PieChart;
-*/
-
-
-import * as d3 from "d3";
-
-class PieChart {
-    createPieChart() {
-        const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
-        const svgWidth = 500, svgHeight = 300;
-        const radius = Math.min(svgWidth, svgHeight) / 2;
-
-        // Wähle das EXISTIERENDE SVG aus (korrekter Klassenname!)
-        const svg = d3.select('.bar-chart')  // Achtung: Kein Tippfehler? ".bar-chart" oder ".bar-chart"?
-            .attr("width", svgWidth)         // Überschreibt die SVG-Größe falls nötig
-            .attr("height", svgHeight)
-            .append("g")                     // Füge eine Gruppe für die Transformation hinzu
-            .attr("transform", `translate(${svgWidth / 2}, ${svgHeight / 2})`);
-
-        const color = d3.scaleOrdinal(d3.schemeCategory10);
-        const pie = d3.pie<number>();
-        const arc = d3.arc<d3.PieArcDatum<number>>()
-            .innerRadius(0)
-            .outerRadius(radius);
-
-        // Zeichne die Pfade (Arcs)
-        svg.selectAll(".arc")
-            .data(pie(dataset))
-            .enter()
-            .append("g")
-            .attr("class", "arc")
-            .append("path")
-            .attr("d", arc)
-            .attr("fill", (d, i) => color(i.toString()));  // Farbe hinzufügen
-    }
-}
-
-// Initialisierung
-document.addEventListener("DOMContentLoaded", () => {
-    new PieChart().createPieChart();
 });
